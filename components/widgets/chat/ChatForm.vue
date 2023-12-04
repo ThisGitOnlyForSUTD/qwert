e8ea556ab749399e83620e493437d31d3b55e9cb
dSkudilo <dskudilo@gmail.com>
2023-12-04 22:04:51 +0300

    add btn

--- components/widgets/chat/ChatForm.vue
+++ components/widgets/chat/ChatForm.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="chat">
-    <ChatButton />
+    <ChatButton
+      v-if="!isChatOpen"
+      @open-chat="isChatOpen = true"
+    />
   </div>
 </template>
-<script setup>
+<script setup lang="ts">
 import ChatButton from '~/components/widgets/chat/ChatButton.vue'
+const isChatOpen = ref(false)
 </script>
 <style lang="scss" scoped>
 .chat {
